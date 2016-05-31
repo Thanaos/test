6a6b9f96679002fc89a8dc02cdff94b7255ff743
xiawenlong <leilei@qq.com>
2016-05-31 16:27:32 +0800

    五个数

---
+++ index3.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * 一个数的组合
+ **/
+$dsn = "mysql:host=localhost;dbname=test";
+$db = new PDO($dsn, 'root', '');
+$sql= "select * from test";
+$rs = $db->query($sql);
+$newarray = array();
+
+while($row = $rs->fetch()){
+    $array = array($row[1], $row[2], $row[3], $row[4], $row[5]);
+    $array = array_values(array_unique($array));
+    $num = count($array);
+    
+    if( $num == 5){
+        $newarray[] = $array[0].$array[1].$array[2].$array[3].$array[4];
+    }
+    
+}
+$sort = array_count_values($newarray);
+arsort($sort);
+var_dump($sort);
+?>
+
+
